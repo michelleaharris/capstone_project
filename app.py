be94e0282c8f9d1aa709505197ea104a13d356e9
Lee Solomon <leesolomon@Lees-MacBook-Pro.local>
2016-09-14 10:05:15 -0400

    week2

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+  return render_template('index.html')
 
-@app.route('/index')
-def index():
-	return render_template('index.html')
+@app.route('/residential')
+def res():
+  return render_template('sfam.html')
+
+@app.route('/commercial')
+def com():
+  return render_template('com.html')
+
+@app.route('/industrial')
+def ind():
+  return render_template('ind.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Sorry, page not found', 404
+
+
 
 
 if __name__ == '__main__':
