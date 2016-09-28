f13b09f1a06fd5f42b9631434731bb72285f11c8
Lee Solomon <leesolomon@Lees-MacBook-Pro.local>
2016-09-28 14:01:57 -0400

    updatedgraphs

--- app.py
+++ app.py
@@ -6,13 +6,45 @@ app = Flask(__name__)
 def main():
   return render_template('index.html')
 
-@app.route('/comparison')
+@app.route('/comparison', methods=['GET','POST'])
 def res():
-  return render_template('sfam.html')
+  	if request.method == 'GET':
+		return render_template('sfam.html')
+	else:
+		app.vars['ticker'] = request.form['ticker'].upper()
+		return redirect('/graph')
 
-@app.route('/explore')
+@app.route('/energymap')
 def com():
   return render_template('com.html')
+
+@app.route('/explore')
+def ind():
+  return render_template('ind.html')
+
+@app.route('/residential')
+def viol():
+  return render_template('violin.html')
+
+@app.route('/graph', methods=['GET'])
+def graph():
+	#Data into Pandas
+	qurl = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?column_index=4&start_date=2016-08-01&api_key=PbjyCkG1dayVcyLQx-si' % app.vars['ticker']
+	r = requests.get(qurl)
+	col = r.json()['dataset']['column_names']
+	data = r.json()['dataset']['data']
+	df = pd.DataFrame(data, columns=col)
+
+	#Bokeh plot
+	p = figure(x_axis_type="datetime", title="August 2016 Closing")
+	p.grid.grid_line_alpha=0.3
+	p.xaxis.axis_label = 'Date'
+	p.yaxis.axis_label = 'Closing Price'
+	p.line(datetime(df['Date']), df['Close'], color='#33A02C')
+
+	script, div = components(p)
+
+	return render_template('graph.html',ticker=app.vars['ticker'], script=script, div=div)
 
 @app.errorhandler(404)
 def page_not_found(error):
